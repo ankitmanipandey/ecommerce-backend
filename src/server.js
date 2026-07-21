68a22b4271310105a342dfe2778d80036051694e
Ankit Mani Pandey <edumusicindia.tech@gmail.com>
2026-07-21 20:25:14 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./database/database");
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server is successfully running on port ${PORT}`);
+    });
+});
